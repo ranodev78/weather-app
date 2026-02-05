3e2efab96b09aa6c0f33683fe676b6f379f3db17
ranodev78 <ranochrono29@gmail.com>
2026-02-04 22:10:11 -0600

    add app location as environment variable

--- src/main/java/com/learning/weather/app/controller/WeatherController.java
+++ src/main/java/com/learning/weather/app/controller/WeatherController.java
@@ -1,5 +1,7 @@
 package com.learning.weather.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1.0/weather")
 public class WeatherController {
+    private final String location;
+
+    @Autowired
+    public WeatherController(@Value("app.location") final String location) {
+        this.location = location;
+    }
 
     @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
     public String test() {
-        return "Hello world!";
+        return "Hello world from %s!".formatted(this.location);
     }
 
     @GetMapping(value = "", produces = MediaType.TEXT_PLAIN_VALUE)
     public String app() {
-        return "Weather app!";
+        return "%s Weather app!".formatted(this.location);
     }
 }
